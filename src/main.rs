b57e7cc0658e6112f56acf797b6e0af6572d02ad
adarshares <adarshares@gmail.com>
2024-07-13 11:06:34 +0530

    stack optimisation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub fn hamming_distance(string1: &Vec<char>, string2: &Vec<char>) -> usize {
+pub fn hamming_distance(string1: &&[u8], string2: &&[u8]) -> usize {
     let mut distance = 0;
     let mut i = 0;
     
@@ -14,12 +14,23 @@ use std::time::Instant;
 fn main() {
     let string1 = "geekspracticehguisdhgiiuweyriuwehiiuweyriuwehfwebfuwebguyuheruihgvuyebghuiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwueyveuyrghfiuhwuefwebfuwebguyuheruiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwueihgvuyebghuyveuyrghfiuhwueihfiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwueiwuehuyfgbwuybfviyuwehbiufh";
     let string2 = "nerdspractiseeruhfiuerhgbgtreughiuewriiuweyriuwehfwebfuwebguyiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwueuheruihgvuyebghuyveuyrghfiuhwuehfiuwiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwueehfuibuyweiiuweyriuwehfwebfuwebguyuheruihgvuyebghuyveuyrghfiuhwuebiughneruijhfoweifjwoeijguibhniurteboehjoirwhjeoifhoou";
-    let string1: Vec<char> = string1.chars().collect();
-    let string2: Vec<char> = string2.chars().collect();
+    let string1 = string1.as_bytes();
+    let string2 = string2.as_bytes();
+    let r1 = &string1;
+    let r2 = &string2;
     let mut x = 0;
     let start = Instant::now();
     for _i in 0..10000000 {
-        x = hamming_distance(&string1, &string2);
+        x = hamming_distance(r1, r2);
+        // x = 0;
+        // let mut i = 0;
+        // while i < string1.len() {
+        //     if string1[i] != string2[i] {
+        //         x += 1;
+        //     }
+        //     i += 1;
+        // }
+        //return distance;
     }
     let duration = start.elapsed();
     println!("{}",x);
